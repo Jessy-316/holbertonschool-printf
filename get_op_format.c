edbbe3cfcb94367c668bf44df7f7c0bb682f4cc8
franciscobaez <9496@holbertonstudents.com>
2024-07-18 13:30:31 -0400

    get_op_format file upload, first push

---
+++ get_op_format.c
@@ -0,0 +1,26 @@
+#include "_printf.h"
+/**
+ * get_op_format - Connects to th ecorrect operating function.
+ * @s: Operator passed as argument to the program.
+ * @va_list: List of arguments.
+ */
+int get_op_format(char *s, va_list)
+{
+	op_t ops[] = {
+		{"c", _putchar},
+		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int},
+		{NULL, NULL},
+	};
+	int i, count = 0;
+
+	for (i = 0; ops[i].op != NULL; i++)
+	{
+		if (s == ops[i].op)
+		{
+			count = _strlen(*s);
+		}
+	}
+	return(*ops[i].f);
+}
