6af1e58148e2802dccf776dd2684b9702a7b2a56
franciscobaez <9496@holbertonstudents.com>
2024-07-22 13:43:32 -0400

    changes made to the string function to handle empty strings

--- _printf.c
+++ _printf.c
@@ -17,7 +17,7 @@ int _printf(const char *format, ...)
 	{
 		for (i = 0; format[i] != '\0'; i++)
 		{
-			if (format[i] == '%' && format[i + 1] != '\0')
+			if (format[i] == '%')
 			{
 				int print = get_op_format(format[i + 1], args);
 
@@ -26,9 +26,9 @@ int _printf(const char *format, ...)
 					count += print;
 					i++;
 				}
-				else
+				else if (format[i + 1] == 's' && print == 0)
 				{
-					count += _putchar(format[i]);
+					i++;
 				}
 			}
 			else
