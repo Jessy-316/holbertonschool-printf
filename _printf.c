5f98fc7f1b9ce7e622c648c1f64065a62c085210
franciscobaez <9496@holbertonstudents.com>
2024-07-23 19:53:17 -0400

    Changes made to the _printf.c file to return 0 if NULL

--- _printf.c
+++ _printf.c
@@ -7,37 +7,35 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
 	int i, count = 0;
+	va_list args;
 
 	va_start(args, format);
 
 	if (format == NULL)
-	{
 		return (0);
-	}
-	else if (format != NULL)
+
+	if (format != NULL)
 	{
 		for (i = 0; format[i] != '\0'; i++)
 		{
-			count += _putchar(format[i]);
-		}
-		if (format[i] == '%')
-		{
-			int print = get_op_format(format[i + 1], args);
+			if (format[i] == '%')
+			{
+				int print = get_op_format(format[i + 1], args);
 
-			if (print != 0)
-			{
-				count += print;
-				i++;
-			}
-			else if (format[i + 1] == 's' && print == 0)
-			{
-				i++;
-			}
-			else
-			{
-				count += _putchar(format[i]);
+				if (print != 0)
+				{
+					count += print;
+					i++;
+				}
+				else if (format[i + 1] == 's' && print == 0)
+				{
+					i++;
+				}
+				else
+				{
+					count += _putchar(format[i]);
+				}
 			}
 			else
 			{
